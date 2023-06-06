1963db4f13ec9b2ea2a97c341a108f7494f4dd2b
Péter Szilágyi <peterke@gmail.com>
2023-06-06 12:53:29 +0300

    core/txpool: make transaction validation reusable across packages (pools) (#27429)
    * core/txpool: abstraction prep work for secondary pools (blob pool)
    
    * core/txpool: leave subpool concepts to a followup pr
    
    * les: fix tests using hard coded errors
    
    * core/txpool: use bitmaps instead of maps for tx type filtering

--- les/test_helper.go
+++ les/test_helper.go
@@ -234,7 +234,7 @@ func newTestServerHandler(blocks int, indexers []*core.ChainIndexer, db zonddb.D
 
 	txpoolConfig := txpool.DefaultConfig
 	txpoolConfig.Journal = ""
-	txpool := txpool.NewTxPool(txpoolConfig, gspec.Config, simulation.Blockchain())
+	txpool := txpool.New(txpoolConfig, gspec.Config, simulation.Blockchain())
 
 	server := &LesServer{
 		lesCommons: lesCommons{
