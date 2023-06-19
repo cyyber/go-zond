1073b379c90d3607627bfd7313c6a83d50d2fcc0
rjl493456442 <garyrong0905@gmail.com>
2023-06-19 22:28:40 +0800

    trie: remove parameter 'fromLevel' in Prove (#27512)
    This removes the feature where top nodes of the proof can be elided.
    It was intended to be used by the LES server, to save bandwidth 
    when the client had already fetched parts of the state and only needed
    some extra nodes to complete the proof. Alas, it never got implemented
    in the client.

--- light/trie.go
+++ light/trie.go
@@ -192,7 +192,7 @@ func (t *odrTrie) GetKey(sha []byte) []byte {
 	return nil
 }
 
-func (t *odrTrie) Prove(key []byte, fromLevel uint, proofDb zonddb.KeyValueWriter) error {
+func (t *odrTrie) Prove(key []byte, proofDb zonddb.KeyValueWriter) error {
 	return errors.New("not implemented, needs client/server interface split")
 }
 
