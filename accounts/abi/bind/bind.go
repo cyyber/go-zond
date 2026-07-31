9d499f3d8348ee42f0a70fb93e160e00a467b6ff
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2026-07-31 19:03:12 +0400

    docs(abi): align function topic comment

--- accounts/abi/bind/bind.go
+++ accounts/abi/bind/bind.go
@@ -343,7 +343,7 @@ func bindTopicType(kind abi.Type, structs map[string]*tmplStruct) string {
 	// parameters that are not value types i.e. arrays and structs are not
 	// stored directly but instead a keccak256-hash of an encoding is stored.
 	//
-	// We convert strings, bytes and functions to hashes, still need to deal with
+	// We only convert strings, bytes and functions to hashes, still need to deal with
 	// array(both fixed-size and dynamic-size) and struct.
 	if bound == "string" || bound == "[]byte" || kind.T == abi.FunctionTy {
 		bound = "common.Hash"
