d3445a064765aaba096de9ae8aaa4835347324ac
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2026-08-01 20:11:02 +0400

    cmd/utils: preserve preimage config values

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1454,8 +1454,9 @@ func SetQRLConfig(ctx *cli.Context, stack *node.Node, cfg *qrlconfig.Config) {
 	if ctx.IsSet(CacheNoPrefetchFlag.Name) {
 		cfg.NoPrefetch = ctx.Bool(CacheNoPrefetchFlag.Name)
 	}
-	// Read the value from the flag no matter if it's set or not.
-	cfg.Preimages = ctx.Bool(CachePreimagesFlag.Name)
+	if ctx.IsSet(CachePreimagesFlag.Name) {
+		cfg.Preimages = ctx.Bool(CachePreimagesFlag.Name)
+	}
 	if cfg.NoPruning && !cfg.Preimages {
 		cfg.Preimages = true
 		log.Info("Enabling recording of key preimages since archive mode is used")
